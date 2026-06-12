65a789feae4160f6a547c08e61ecd6bd19f02cb1
Z User <z@container>
2026-06-12 11:14:49 +0000

    Clean up: remove screenshots, downloads, temp files from repo

--- keep-alive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  npx next dev -p 3000 2>&1 | tee dev.log
-  echo "Server died at $(date), restarting in 3s..." >> /tmp/next-restarts.log
-  sleep 3
-done
